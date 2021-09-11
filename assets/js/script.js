a86e801f4259d9035e4232103e96b211be8e03f3
Alexis H <haleeexis@gmail.com>
2021-09-11 14:30:54 -0400

    mobile layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
   $(".clickable").on("click", function (event) {
-    // event.stopPropagation();
-    // event.stopImmediatePropagation();
     event.preventDefault();
     $(this).next().toggle();
     $(this).css("color", "#228B22");
@@ -9,7 +7,5 @@ $(document).ready(function () {
       console.log("here");
       $(this).css("color", "black");
     }
-
-    // The same works with hidden
   });
 });
