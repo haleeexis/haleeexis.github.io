bc5ffdaf9e9c303dae31d2f48aef02bdd4698f72
Alexis H <haleeexis@gmail.com>
2021-09-18 17:59:27 -0400

    highlight

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,18 @@ $(document).ready(function () {
   $(".clickable").on("click", function (event) {
     event.preventDefault();
     $(this).next().toggle();
-    $(this).css("color", "#228B22");
+    // $(this).css(".clickable span", "background-color: green;color: white;");
+    $(this).find(">:first-child").css({
+      "background-color": "green",
+      color: "white",
+    });
     if ($(this).next().css("display") == "none") {
       console.log("here");
       $(this).css("color", "black");
+      $(this).find(">:first-child").css({
+        "background-color": "transparent",
+        color: "black",
+      });
     }
   });
 });
